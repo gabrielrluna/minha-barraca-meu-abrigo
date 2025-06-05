277fa67a0b7b47b055ace980758f7af0e157bc03
Gabriel Luna <lunagabri@gmail.com>
2025-06-05 14:45:51 -0300

    revisão de imagens

--- js/script.js
+++ js/script.js
@@ -4,20 +4,23 @@ const unidades = [
     "idade":"10 anos",
     "conselheiros":"xyz",
     "instrutores":"xyzda",
-    "grito":""
+    "logo":"logo-nebulosa.webp",
+    "grito":"A nossa unidade chegou para ficar<br>Á órion nebulosa vem aqui pre te mostrar<br>Que a nossa unidade é garra e tradição<br>Seguindo o objetivo e o plano da salvação<br>Que venham os problemas<br>Que venha a provação<br>Pois a nossa unidade é garra e tradição<br>Orion nebulosa novo jeito de ser<br>Na nossa unidade o importante é aprender<br>Orion nebulosa! É garra e tradição!<br>Orion nebulosa! Moral do batalhão!<br>Nebu nebu nebulosa oh!"
   },
   {
     "unidade":"Rigel",
     "idade":"11 anos",
     "conselheiros":"xyz",
     "instrutores":"xyzda",
-    "grito":"xyzda"
+    "logo":"logo-rigel.webp",
+    "grito":"A rigel ninguem pega!<br>A rigel ninguem segura!<br>O que ela é? A mais unida!<br>O que ela tem? A mais temida!<br>Força, garra, coragem, uniao e mais dedicação!<br>Unidade Rigel invadindo a região!<br>eeeeee rigel!"
   },
   {
     "unidade":"Bellatrix",
     "idade":"12 anos",
     "conselheiros":"xyz",
     "instrutores":"xyzda",
+    "logo":"logo-bellatrix.webp",
     "grito":"Moral! Moral! Moral! Moral! Moral!<br>Moral do batalhão! Bellatrix é a força, poder e União!<br>Bellatrix é moral! Moral do batalhão!<br> Guerra nosso lema, vontade e tradição!<br>E na nossa unidade não tem moleza não!<br>Na terra, no céu, seja onde for,<br>Unidade Bellatrix mostrando seu Valor!<br>Bella! Bella! Bellatrix!<br>Fé em Deus que Ele é justo! (2x)<br> Se Deus é por nós, quem será contra nós (2x)<br>Unidos Venceremos (2x)<br>1, 2, 3, Bella!"
   },
   {
@@ -25,60 +28,66 @@ const unidades = [
     "idade":"13 e 14 anos",
     "conselheiros":"xyz",
     "instrutores":"xyzda",
-    "grito":"xyzda"
+    "logo":"logo-saiph2.webp",
+    "grito":"No céu a estrela de uma constelação<br>Na terra unidade, poder e união<br>Tudo posso quando for conquistar com tradição<br>Saiph guerreira no nosso coração<br>Brilha unidade, Moral na Nebulosa<br>Ataca com garra, estrela fabulosa<br>Desbrava Aguerrida não importa onde for<br>Unidade Saiph, Disciplina e valor<br>Saiph oh!"
   },
   {
     "unidade":"Sigma",
     "idade":"15 anos",
     "conselheiros":"xyz",
     "instrutores":"xyzda",
-    "grito":"xyzda"
+    "logo":"logo-sigma.webp",
+    "grito":"Sigma em campo<br>Veio mostrar o seu valor<br>Combatendo o bom combate<br>Veio expalhar o seu calor<br>CAPITÃO: Na tua força,<br>TODOS: imponência<br>CAPITÃO: Em sua luta,<br>TODOS: tem vitória<br>CAPITÃO: Na vitória,<br>TODOS: tua crença<br>CAPITÃO: Em tua crença,<br>TODOS: vem a glória!Força e imponência - CAPITÃO<br>SIGMA - TODOS"
   },
   {
     "unidade":"Hatsya",
     "idade":"10 anos",
     "conselheiros":"xyz",
     "instrutores":"xyzda",
-    "grito":"xyzda"
+    "logo":"logo-hatsya.webp",
+    "grito":"Ha razão para guiar o coração<br>Te conhecer para vitória obter<br>Sabedoria para viver em alegria<br>Essa unidade que nos contagia<br>Servindo nossa casa, nosso Deus e nosso clube<br>Como unidade não a quem nos derrube<br>Ha Ha Hatsya!"
   },
   {
     "unidade":"Meissa",
     "idade":"11 anos",
     "conselheiros":"xyz",
     "instrutores":"xyzda",
-    "grito":"xyzda"
+    "logo":"logo-meissa.webp",
+    "grito":"Me! Meninas lindas pode crer<br>I! Inteligência e valor!<br>Sa! Sabendo o que eu vou dizer<br>A Meissa veio aqui pra ganhar, pra vencer, pra mostrar que vai detonar!<br>Dominou! Dominou! Já ganhou! Já ganhou!<br>Animou! Animou e gritou:<br>Meissa!"
   },
   {
     "unidade":"Alnilan",
     "idade":"12 anos",
     "conselheiros":"xyz",
     "instrutores":"xyzda",
-    "grito":"xyzda"
+    "logo":"logo-alnilan.webp",
+    "grito":"Alnilan é linda, linda!<br>Alnilan é poderosa, poderosa!<br>Alnilan tem carisma, tem carisma!<br>Alnilan é diva, diva!<br>Vai no acampamento, Missão Órion sem chapinha!<br>Alnilan é linda! 2x<br>Na lama ou caminhada, sem moleza na vigília!<br>Alnilan é linda! (2x)<br>Jesus é nosso guia, serviremos com alegria!<br>Alnilan é linda! (2x)<br>Pregando o evangelho, continuamos sempre unidas!<br>Alnilan é linda! (2x)<br>Alnilan é feia? Não!<br>Alnilan é fraca? Não!<br>Então o que a melhor unidade do clube órion é?<br>LINDA!"
   },
   {
     "unidade":"Alnitak",
     "idade":"13 e 14 anos",
     "conselheiros":"xyz",
     "instrutores":"xyzda",
-    "grito":"xyzda"
+    "logo":"logo-alnitak.webp",
+    "grito":"Al Alguém está gritando aí<br>Ni Ninguém vai poder escapar<br>Tak Tá aqui um grito pra valer<br>Vamos juntar as letras e dizer<br>Quem pode, pode<br>Quem não pode, vem também<br>Com a unidade que só quer fazer o bem<br>Alnitak, unidade pra valer<br>Se quiser sorrir, se quiser vencer<br>Meninas graciosas como nós não tem igual<br>Então segura firme para o grito final:<br>Alnitak!!"
   },
   {
     "unidade":"M78",
     "idade":"15 anos",
     "conselheiros":"xyz",
     "instrutores":"xyzda",
-    "grito":"xyzda"
+    "logo":"logo-m78.webp",
+    "grito":"Alô clube Orion, eu não quero me gabar<br>Mais quando a gente chega faz o clube balançar,<br>Uhu abalou M78 chegou,<br>pra mostrar o seu brilho e provar o seu valor<br>A M78 não para não<br>Cantamos, gritamos<br>Com todo coração<br>M78!"
   },
 
 ]
 
 function unidadeModal(unidadeNum){
   var unidadeAr = unidades[unidadeNum];
-    // console.log(unidadeAr.unidade);
-
   $(".modal-title").html("Unidade: "+unidadeAr.unidade)
   $("#conteudoModal").html("<p><b>Unidade: "+unidadeAr.unidade+"</b></p><p><b></b>Idade: "+unidadeAr.idade+"</b></p><p><b></b>Conselheiros: "+unidadeAr.conselheiros+"</b></p><p><b></b>Instrutores: "+unidadeAr.instrutores+"</b></p>");
   $("#grito").html("<h4>Grito de Guerra</h4>"+unidadeAr.grito);
+  $("#logoUnidade").html("<img src='/assets/img/logo/"+unidadeAr.logo+"' class='img-fluid' alt=''>")
 
 }
 
